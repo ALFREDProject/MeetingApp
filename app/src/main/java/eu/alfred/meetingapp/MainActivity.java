24af9981398cbdb4cd81e2996b768245e386bab5
Deniz Coskun 2 <deniz.coskun@mbptie01.local>
2016-02-29 14:02:50 +0100

    Android project files added to repository

---
+++ app/src/main/java/eu/alfred/meetingapp/MainActivity.java
@@ -0,0 +1,13 @@
+package eu.alfred.meetingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
